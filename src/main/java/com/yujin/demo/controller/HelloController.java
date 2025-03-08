9671976f7060385578a146acb17a694d677d76bb
상어 <tkddj814@naver.com>
2025-03-08 19:10:20 +0900

    init project

---
+++ src/main/java/com/yujin/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.yujin.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello Yujin!";
+	} 
+}
